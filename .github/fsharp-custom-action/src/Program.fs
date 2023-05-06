764f3e9a93a92374d1a8d5b07222e0673909530c
64J0 <vinigaio97@gmail.com>
2023-05-06 10:37:13 -0300

    lint: src/Program.fs

--- .github/fsharp-custom-action/src/Program.fs
+++ .github/fsharp-custom-action/src/Program.fs
@@ -7,16 +7,13 @@ open Newtonsoft.Json
 
 let GITHUB_OUTPUT_FILE = "GITHUB_OUTPUT"
 
-let private removeSquareBrackets (s: string): string =
-    Regex.Replace(s, "\[|\]", "")
+let private removeSquareBrackets (s: string) : string = Regex.Replace(s, "\[|\]", "")
 
-let private removeDoubleQuotes (s: string): string =
-    s.Replace("\"", "")
+let private removeDoubleQuotes (s: string) : string = s.Replace("\"", "")
 
-let private removeBackslash (s: string): string =
-    s.Replace("\\", "")
+let private removeBackslash (s: string) : string = s.Replace("\\", "")
 
-let getDirectory (s: string): string =
+let getDirectory (s: string) : string =
     let dirsNameArray = s.Split [| '/' |]
 
     match (dirsNameArray.Length > 3) with
@@ -24,31 +21,28 @@ let getDirectory (s: string): string =
     | false -> ""
 
 let getFormattedDirectories (args: string array) =
-    (args.[0]
-     |> removeSquareBrackets
-     |> removeDoubleQuotes
-     |> removeBackslash).Split [| ',' |]
-     |> Array.map getDirectory
-     |> Array.distinct
+    (args.[0] |> removeSquareBrackets |> removeDoubleQuotes |> removeBackslash)
+        .Split
+        [| ',' |]
+    |> Array.map getDirectory
+    |> Array.distinct
 
-let getserializedResult (result: string array): string =
-    JsonConvert.SerializeObject result
+let getserializedResult (result: string array) : string = JsonConvert.SerializeObject result
 
-let private getGithubOutputPath (): string =
+let private getGithubOutputPath () : string =
     Environment.GetEnvironmentVariable(GITHUB_OUTPUT_FILE)
 
 // https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action#writing-the-action-code
-let private writeToGithubOutput (serializedResult: string): unit =
-    let githubOutputPath = getGithubOutputPath()
+let private writeToGithubOutput (serializedResult: string) : unit =
+    let githubOutputPath = getGithubOutputPath ()
     printfn "githubOutputPath: %s" githubOutputPath
 
     match (File.Exists githubOutputPath) with
     | false -> () // when running outside Github Actions environment
-    | true ->
-        File.AppendAllText(githubOutputPath, $"dirschanged={serializedResult}")
+    | true -> File.AppendAllText(githubOutputPath, $"dirschanged={serializedResult}")
 
 [<EntryPoint>]
-let main (args: string array): int =
+let main (args: string array) : int =
     let result = getFormattedDirectories args
     let serializedResult = getserializedResult result
     printfn "serializedResult: %s" serializedResult
