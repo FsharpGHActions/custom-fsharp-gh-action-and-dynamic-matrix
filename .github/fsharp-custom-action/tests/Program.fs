04cb5dfc3f18390c54ca72dd4a51dd3f78ff4fff
64J0 <vinigaio97@gmail.com>
2023-05-06 10:37:54 -0300

    lint: tests/Program.fs

--- .github/fsharp-custom-action/tests/Program.fs
+++ .github/fsharp-custom-action/tests/Program.fs
@@ -5,45 +5,56 @@ open GitHubActionMain
 
 let zeroDirZeroSize = "[]"
 let oneDirOneSize = "[\"terraform/instance-1/network/main.tf\"]"
-let oneDirTwoSize = "[\"terraform/instance-1/network/main.tf\",\"terraform/instance-1/network/outputs.tf\"]"
-let twoDirThreeSizeArray = "[\"terraform/instance-1/network/main.tf\",\"terraform/instance-1/network/outputs.tf\",\"terraform/instance-2/virtual-machine/main.tf\"]"
+
+let oneDirTwoSize =
+    "[\"terraform/instance-1/network/main.tf\",\"terraform/instance-1/network/outputs.tf\"]"
+
+let twoDirThreeSizeArray =
+    "[\"terraform/instance-1/network/main.tf\",\"terraform/instance-1/network/outputs.tf\",\"terraform/instance-2/virtual-machine/main.tf\"]"
 
 [<Tests>]
 let tests =
-    testList "Different input scenarios" [
-        test "Empty array input" {
-            let result = getFormattedDirectories [| zeroDirZeroSize |]
-            Expect.equal result [| "" |] "The strings should equal"
+    testList
+        "Different input scenarios"
+        [ test "Empty array input" {
+              let result = getFormattedDirectories [| zeroDirZeroSize |]
+              Expect.equal result [| "" |] "The strings should equal"
 
-            let serializedResult = getserializedResult result
-            Expect.equal serializedResult "[\"\"]" "The strings should equal"
-        }
+              let serializedResult = getserializedResult result
+              Expect.equal serializedResult "[\"\"]" "The strings should equal"
+          }
 
-        test "Single directory one-size array input" {
-            let result = getFormattedDirectories [| oneDirOneSize |]
-            Expect.equal result [| "terraform/instance-1/network" |] "The strings should equal"
+          test "Single directory one-size array input" {
+              let result = getFormattedDirectories [| oneDirOneSize |]
+              Expect.equal result [| "terraform/instance-1/network" |] "The strings should equal"
 
-            let serializedResult = getserializedResult result
-            Expect.equal serializedResult "[\"terraform/instance-1/network\"]" "The strings should equal"
-        }
+              let serializedResult = getserializedResult result
+              Expect.equal serializedResult "[\"terraform/instance-1/network\"]" "The strings should equal"
+          }
 
-        test "Single directory two-size array input" {
-            let result = getFormattedDirectories [| oneDirTwoSize |]
-            Expect.equal result [| "terraform/instance-1/network" |] "The strings should equal"
+          test "Single directory two-size array input" {
+              let result = getFormattedDirectories [| oneDirTwoSize |]
+              Expect.equal result [| "terraform/instance-1/network" |] "The strings should equal"
 
-            let serializedResult = getserializedResult result
-            Expect.equal serializedResult "[\"terraform/instance-1/network\"]" "The strings should equal"
-        }
+              let serializedResult = getserializedResult result
+              Expect.equal serializedResult "[\"terraform/instance-1/network\"]" "The strings should equal"
+          }
 
-        test "Two directories three-size array input" {
-            let result = getFormattedDirectories [| twoDirThreeSizeArray |]
-            Expect.equal result [| "terraform/instance-1/network"; "terraform/instance-2/virtual-machine" |] "The strings should equal"
+          test "Two directories three-size array input" {
+              let result = getFormattedDirectories [| twoDirThreeSizeArray |]
 
-            let serializedResult = getserializedResult result
-            Expect.equal serializedResult "[\"terraform/instance-1/network\",\"terraform/instance-2/virtual-machine\"]" "The strings should equal"
-        }
-    ]
+              Expect.equal
+                  result
+                  [| "terraform/instance-1/network"; "terraform/instance-2/virtual-machine" |]
+                  "The strings should equal"
+
+              let serializedResult = getserializedResult result
+
+              Expect.equal
+                  serializedResult
+                  "[\"terraform/instance-1/network\",\"terraform/instance-2/virtual-machine\"]"
+                  "The strings should equal"
+          } ]
 
 [<EntryPoint>]
-let main args =
-    runTests defaultConfig tests
+let main args = runTests defaultConfig tests
